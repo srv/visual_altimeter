57f5b67b747e93f41ebb994abf582cf78e6edee5
Stephan Wirth <stephan.wirth@uib.es>
2012-10-01 18:44:57 +0200

    fixed 0 count output

--- src/visual_altimeter.cpp
+++ src/visual_altimeter.cpp
@@ -29,37 +29,34 @@ public:
 
   void publishMeanDistance(const PointCloud::ConstPtr& point_cloud)
   {
+    double mean_z, min_z, max_z;
+    mean_z = 0.0;
+    max_z = 0.0;
+    min_z = std::numeric_limits<double>::max();
+    int count = 0;
+    for (size_t i = 0; i < point_cloud->points.size(); ++i)
+    {
+      double z = point_cloud->points[i].z;
+      if (!std::isnan(z))
+      {
+        mean_z += z;
+        if (z < min_z) min_z = z;
+        if (z > max_z) max_z = z;
+        count++;
+      }
+    }
     std_msgs::Float32 dist_msg;
-    if (point_cloud->points.size() == 0)
+    if (count == 0)
     {
       dist_msg.data = -1;
     }
     else
     {
-      double mean_z, min_z, max_z;
-      mean_z = 0.0;
-      max_z = 0.0;
-      min_z = std::numeric_limits<double>::max();
-      int count = 0;
-      for (size_t i = 0; i < point_cloud->points.size(); ++i)
-      {
-        double z = point_cloud->points[i].z;
-        if (!std::isnan(z))
-        {
-          mean_z += z;
-          if (z < min_z) min_z = z;
-          if (z > max_z) max_z = z;
-          count++;
-        }
-      }
-      mean_z =  mean_z / count;
-
-      ROS_INFO_STREAM("   Z: MIN: " << min_z << "\tMAX: " << max_z << " \tMEAN: " << mean_z);
-      dist_msg.data = mean_z;
+      dist_msg.data = mean_z / count;
     }
+    ROS_INFO_STREAM("   Z: MIN: " << min_z << "\tMAX: " << max_z << " \tMEAN: " << mean_z);
     mean_dist_pub_.publish(dist_msg);
   }
-
 };
 
 int main(int argc, char** argv)
