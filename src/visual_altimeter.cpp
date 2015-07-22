32dc312e7397b60f975a861410f6bec5cdb7303a
plnegre <plnegre@binibook.com>
2015-07-22 12:48:55 +0200

    Add a new security check

--- src/visual_altimeter.cpp
+++ src/visual_altimeter.cpp
@@ -33,13 +33,13 @@ class VisualAltimeterNode
 public:
   VisualAltimeterNode() : nh_private_("~")
   {
-    nh_private_.param("min_range", min_range_, 0.2);
-    nh_private_.param("max_range", max_range_, 4.0);
+    nh_private_.param("min_range", min_range_, 0.5);
+    nh_private_.param("max_range", max_range_, 5.0);
     nh_private_.param("field_of_view", field_of_view_, 60.0/180.0*M_PI);
-    nh_private_.param("min_x", min_x_, -0.2);
-    nh_private_.param("max_x", max_x_,  0.2);
-    nh_private_.param("min_y", min_y_, -0.2);
-    nh_private_.param("max_y", max_y_,  0.2);
+    nh_private_.param("min_x", min_x_, -0.4);
+    nh_private_.param("max_x", max_x_,  0.4);
+    nh_private_.param("min_y", min_y_, -0.4);
+    nh_private_.param("max_y", max_y_,  0.4);
     point_cloud_sub_ = nh_.subscribe<PointCloud>("point_cloud", 1, &VisualAltimeterNode::pointCloudCb, this);
     range_pub_ = nh_private_.advertise<sensor_msgs::Range>("altitude", 1);
   }
@@ -48,7 +48,6 @@ public:
   {
     if(range_pub_.getNumSubscribers() == 0) return;
 
-    int count = 0;
     vector<double> z_dist;
     for (size_t i = 0; i < point_cloud->points.size(); ++i)
     {
@@ -57,26 +56,30 @@ public:
           point.y >= min_y_ && point.y <= max_y_ &&
           !isnan(point.z))
       {
-        count++;
         z_dist.push_back(point.z);
       }
     }
 
     // Sanity check and filtering
     double altitude = -1;
-    if (count > 300) // Minimum number of samples
+    if (z_dist.size() > 300)
     {
-      double mean = accumulate(z_dist.begin(), z_dist.end(), 0.0) / z_dist.size();
-
-      // Filter (5 samples)
-      if (filter_.size() < 5)
-        filter_.push_back(mean);
-      else
+      double max_val = *max_element(z_dist.begin(), z_dist.end());
+      double min_val = *min_element(z_dist.begin(), z_dist.end());
+      if (fabs(max_val - min_val) < 0.5)
       {
-        rotate(filter_.begin(), filter_.begin() + 1, filter_.end());
-        filter_[4] = mean;
+        double mean = accumulate(z_dist.begin(), z_dist.end(), 0.0) / z_dist.size();
+
+        // Filter (5 samples)
+        if (filter_.size() < 5)
+          filter_.push_back(mean);
+        else
+        {
+          rotate(filter_.begin(), filter_.begin() + 1, filter_.end());
+          filter_[4] = mean;
+        }
+        altitude = accumulate(filter_.begin(), filter_.end(), 0.0) / filter_.size();
       }
-      altitude = accumulate(filter_.begin(), filter_.end(), 0.0) / filter_.size();
     }
 
     // Publish if valid altitude
@@ -90,7 +93,7 @@ public:
       range_msg.range = altitude;
       range_pub_.publish(range_msg);
     } else {
-      ROS_INFO_THROTTLE(10, "[VisualAltimeter]: Invalid altitude. Number of samples: %d", count);
+      ROS_INFO_THROTTLE(10, "[VisualAltimeter]: Invalid altitude");
     }
   }
 };
